62d7228ccc7b71fe743817b8f473cc259d7a027e
ANISSA <anissamiloudi@hotmail.nl>
2018-01-16 17:26:45 +0100

    One to One association Profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
-         has_many :events
+  has_many :events
+  has_one :profile
+
+  def full_name
+    return profile.full_name if profile?
+
+    email
+  end
+
+  def profile?
+    profile.present? && profile.persisted?
+  end
 end
