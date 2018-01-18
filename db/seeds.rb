4497231721c2f3399d3eb7862ac6237c57145b19
ANISSA <anissamiloudi@hotmail.nl>
2018-01-18 12:19:18 +0100

    Create Photo: Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
+Photo.destroy_all
 Category.destroy_all
+Event.destroy_all
+Profile.destroy_all
 User.destroy_all
 
 
@@ -16,7 +19,7 @@ wouter = User.create(email: "wouter@codaisseurbnb.com", password: "abcd1234")
 matt = User.create(email: "matt@codaisseurbnb.com", password: "abcd1234")
 bla = User.create(email: 'bla@codaisseur.com',password: '123456')
 
-Event.create(
+event1 = Event.create(
   name: "Karate",
   location: "Haarlem",
   starts_at: Time.now,
@@ -32,7 +35,7 @@ Event.create(
 )
 
 
-Event.create!(
+event2 = Event.create!(
   name: "Pancakes",
   location: "New York",
   starts_at: Time.now,
@@ -48,7 +51,7 @@ Event.create!(
 )
 
 
-Event.create!(
+event3 = Event.create!(
   name: "Workshop of Shooting Films",
   location: "Almere",
   starts_at: Time.now,
@@ -62,3 +65,11 @@ Event.create!(
   user: wouter,
   categories: [film, tech]
 )
+
+
+photo1 = Photo.create!(remote_image_url:"http://res.cloudinary.com/amiloudi/image/upload/v1516272579/karate_c809gr.jpg", event: event1)
+photo2 = Photo.create!(remote_image_url:"http://res.cloudinary.com/amiloudi/image/upload/v1516272579/sportsandfitness_umljjr.jpg", event: event1)
+photo3 = Photo.create!(remote_image_url:"http://res.cloudinary.com/amiloudi/image/upload/v1516272564/family_xm2nii.jpg", event: event2)
+photo4 = Photo.create!(remote_image_url:"http://res.cloudinary.com/amiloudi/image/upload/v1516272564/foodanddrinks_cm8myb.jpg", event: event2)
+photo5 = Photo.create!(remote_image_url:"http://res.cloudinary.com/amiloudi/image/upload/v1516272564/film_oo4iam.png", event: event3)
+photo6 = Photo.create!(remote_image_url:"http://res.cloudinary.com/amiloudi/image/upload/v1516272579/tech_xkz4tb.jpg", event: event3)
